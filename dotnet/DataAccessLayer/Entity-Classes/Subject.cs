750e2aaa1c0f12465b64227648bbefd8e7fc3556
Kseniia Kravchenko <kkravchenko@kse.org.ua>
2024-07-15 22:56:49 +0300

    Implemented Subject class

---
+++ dotnet/DataAccessLayer/Entity-Classes/Subject.cs
@@ -0,0 +1,8 @@
+namespace DataAccessLayer.Entity_Classes;
+
+public class Subject
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Teacher { get; set; }
+}
